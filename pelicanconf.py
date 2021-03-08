e8f4d75312ee900b6026d17d38770f33bf5c11dc
Erik Dains <eadains@gmail.com>
2021-03-07 18:57:41 -0600

    maintenance

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'Erik Dains'
-SITENAME = 'Optionally Bayes'
-SITETITLE = 'Optionally Bayes'
-SITESUBTITLE = 'My journey in stock market trading and Bayesian statistics.'
-SITEURL = 'https://eadains.github.io/OptionallyBayes'
-PATH = 'content'
+AUTHOR = "Erik Dains"
+SITENAME = "Erik's Blog"
+SITETITLE = "Erik's Blog"
+SITESUBTITLE = "My journey in life and math. Anything goes."
+SITEURL = "https://eadains.github.io/OptionallyBayes"
+PATH = "content"
 MAIN_MENU = True
-COPYRIGHT_NAME = 'Erik Dains'
-COPYRIGHT_YEAR = '2020'
-# SITELOGO = SITEURL + '/images/integral_logo.png'
+COPYRIGHT_NAME = "Erik Dains"
+COPYRIGHT_YEAR = "2020"
+# SITELOGO = SITEURL + "/images/integral_logo.png"
 
-TIMEZONE = 'America/Chicago'
+TIMEZONE = "America/Chicago"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
-THEME = '/home/eadains/flex_theme'
+THEME = "/home/eadains/flex_theme"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -26,21 +26,21 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-#LINKS = (('Pelican', 'https://getpelican.com/'),
-#         ('Python.org', 'https://www.python.org/'),
-#         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-#         ('You can modify those links in your config file', '#'),)
+#LINKS = (("Pelican", "https://getpelican.com/"),
+#         ("Python.org", "https://www.python.org/"),
+#         ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#         ("You can modify those links in your config file", "#"),)
 
 # Social widget
 SOCIAL = (
-    ('github', 'https://github.com/eadains'),
-    ('instagram', 'https://www.instagram.com/erikdains/')
+    ("github", "https://github.com/eadains"),
+    ("instagram", "https://www.instagram.com/erikdains/")
 )
 
 MENUITEMS = (
-    ('Archives', 'archives.html'),
-    ('Categories', 'categories.html'),
-    ('Tags', 'tags.html')
+    ("Archives", "archives.html"),
+    ("Categories", "categories.html"),
+    ("Tags", "tags.html")
 )
 
 DEFAULT_PAGINATION = 10
